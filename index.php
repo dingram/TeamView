7252f8e59bfc08659b10affaf460602aa1b10103
Dave Ingram <dave@dmi.me.uk>
2010-12-02 17:27:50 +0000

    Reorder blocks for CSS and fix typo in opening tag

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
-<htm>
+<html>
   <head>
 	  <title>TeamView</title>
     <link rel="stylesheet" type="text/css" href="TeamView.css" />
   </head>
   <body>
-    
+
     <div id="HeaderDiv">
-    	
+
     	<?php echo file_get_contents( "Header.shtml" ); ?>
-    	
+
     </div>
-    
-    <div id="TreeDiv">
-    	
-    	<?php echo file_get_contents( "RefTree.shtml" ); ?>
-    	
-    </div>
-    
+
     <div id="SidebarDiv">
-    	
+
     	<?php echo file_get_contents( "Sidebar.shtml" ); ?>
-    	
+
     </div>
-    
+
+    <div id="TreeDiv">
+
+    	<?php echo file_get_contents( "RefTree.shtml" ); ?>
+
+    </div>
+
   </body>
 </html>
